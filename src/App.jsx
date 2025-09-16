f2cbd3cf5980da2cf59d577bb94c54df6801dafe
pritam <pritamsuryawanshi1121@gmail.com>
2025-09-16 20:31:46 +0530

    feat: add dynamic loader using Lucide React

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 import Home from './pages/home';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Layout from "./layout/Layout.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout.jsx';
+import { Loader } from 'lucide-react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <Router>
-
-        <nav>
-          {/* <Link to="/">Home</Link> */}
-        </nav>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </Router>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen bg-gray-900">
+          <Loader className="animate-spin text-purple-600 w-12 h-12" />
+        </div>
+      ) : (
+        <Router>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+        </Router>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
